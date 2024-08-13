475535d1f9878db5743dbd1408a6219c3682e916
SSUDNG <ssudng2716@gmail.com>
2024-08-13 16:58:41 +0900

    fix: commitLint

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
     <>
       <div className="text-display-lg text-red-30">display-lg</div>
       <div className="text-display-lg-m text-red-30">display-lg-m</div>
+      <div className="text-display-lg-m text-red-30">display-lg-m</div>
     </>
   )
 }
