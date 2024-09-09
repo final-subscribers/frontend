6b8e63cf6d976ca921fbfacfae8cc032152ea9cc
Kim Yeojin <duwls1503@gmail.com>
2024-09-09 18:32:57 +0900

    feat: mainPage 추가 & pagination 수정

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,13 +1,16 @@
 import DefaultPagination from '@/components/common/DefaultPagination';
+import { InputWithExtras } from '@/components/common/InputWithExtras';
 import ItemCard from '@/components/common/ItemCard';
 import ItemList from '@/components/common/ItemList';
+import { Button } from '@/components/ui/button';
 import useResponsive from '@/hooks/useResponsive';
+import { CaretRight, MagnifyingGlass } from '@phosphor-icons/react';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { useState } from 'react';
 
 const Main = () => {
-  const { isTablet, isMobile } = useResponsive();
+  const { isDesktop, isTablet, isMobile } = useResponsive();
   const [currentPage, setCurrentPage] = useState(1);
 
   const fetchProperties = async (page: any) => {
@@ -23,49 +26,132 @@ const Main = () => {
   const totalPages = data?.totalPages || 1;
 
   return (
-    <div>
+    <div className="py-12 mobile:py-9">
       <div>{/* swiper */}</div>
-      <div className="w-[1200px] h-full m-auto">
-        <div>
-          <div>{/* 검색 */}</div>
-          <div>{/* 혜택/주변 */}</div>
-          <div>{/* 매물등록 */}</div>
+      <div className="max-w-[1200px] h-full m-auto tablet:mx-7 mobile:mx-5">
+        <div className="flex flex-col items-center gap-9 desktop:gap-11 mb-9 tablet:mb-11">
+          <div className="w-[976px] tablet:w-[677px] mobile:w-[328px]">
+            <InputWithExtras
+              type="text"
+              placeholder="어떤 미분양 매물을 찾으세요?"
+              className={`w-full px-7 py-4 ${isDesktop ? 'px-9 py-7 text-label-lg' : 'text-label-base'} mobile:text-label-base-m border-2 border-primary-default rounded-10 focus:border-2 focus:border-primary-default focus:shadow-default`}
+              trailingExtra={
+                <MagnifyingGlass
+                  size={isDesktop ? 32 : 24}
+                  weight="thin"
+                  className="text-primary-default cursor-pointer"
+                />
+              }
+            />
+          </div>
+          <div className="flex gap-10 tablet:gap-6 mobile:gap-3">
+            <div className="flex mobile:block w-[531px] h-[260px] tablet:w-[328px] tablet:h-[146px] mobile:w-[160px] mobile:h-[167px] p-7 desktop:px-[58px] desktop:py-[52px] mobile:px-5 rounded-7 desktop:rounded-9 shadow-[0_0_20px_0] shadow-effect-shadow">
+              <div className="grow">
+                <div className="mb-3 desktop:mb-5 mobile:text-center">
+                  <span className="text-body-lg tablet:text-body-sm mobile:text-body-sm-m font-bold text-accent-strong">
+                    할인혜택
+                  </span>
+                  <p className="text-heading-base tablet:text-title-lg mobile:text-label-lg-m font-bold">
+                    혜택 줍줍
+                  </p>
+                </div>
+                <div className="mobile:hidden text-body-lg tablet:text-detail-sm text-assistive-strong">
+                  <p>할인 분양부터 옵션 제공까지</p>
+                  <p>미분양 혜택의 모든 것!</p>
+                </div>
+              </div>
+              <div className="flex grow items-end justify-end mobile:justify-center">
+                <img
+                  src="/src/assets/image/benefitImage.png"
+                  alt="benefitImage"
+                  className="w-[137px] h-[131px] tablet:w-[91px] tablet:h-[87px] mobile:w-[70px] mobile:h-[66px]"
+                />
+              </div>
+            </div>
+            <div className="flex mobile:block w-[531px] h-[260px] px-[58px] tablet:w-[328px] tablet:h-[146px] mobile:w-[160px] mobile:h-[167px] py-[37px] tablet:p-7 mobile:px-5 mobile:py-7 rounded-7 desktop:rounded-9 shadow-[0_0_20px_0] shadow-effect-shadow">
+              <div className="grow">
+                <div className="mb-3 desktop:mb-5 mobile:text-center">
+                  <span className="text-body-lg tablet:text-body-sm mobile:text-body-sm-m font-bold text-primary-default">
+                    편의시설
+                  </span>
+                  <p className="text-heading-base tablet:text-title-lg mobile:text-label-lg-m font-bold">
+                    주변 핵심 체크
+                  </p>
+                </div>
+                <div className="mobile:hidden text-body-lg tablet:text-detail-sm text-assistive-strong">
+                  <p>놓치기 쉬운 주변 정보를</p>
+                  <p>키워드로 한눈에!</p>
+                </div>
+              </div>
+              <div className="flex grow items-end justify-end mobile:justify-center">
+                <img
+                  src="/src/assets/image/infraImage.png"
+                  alt="benefitImage"
+                  className="w-[107px] h-[130px] tablet:w-[68px] tablet:h-[81px] mobile:w-[54px] mobile:h-[64px]"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="mobile:block flex w-full h-[162px] mobile:h-[137px] px-[44px] py-[22px] bg-assistive-base rounded-6 desktop:rounded-4">
+            <div className="flex flex-col justify-center mobile:items-center w-full ml-6 mobile:ml-0 mobile:mb-4">
+              <p className="text-title-xl mobile:text-label-lg-m font-bold">
+                미분양 매물을 간편하게 등록하고 싶다면?
+              </p>
+              <p className="text-body-base mobile:text-label-sm-m mt-3 mobile:mt-0">
+                매물 관리부터 고객 관리까지 한번에
+              </p>
+            </div>
+            <div className="flex items-end justify-end mobile:justify-center">
+              <Button
+                variant="assistive"
+                size={`${isMobile ? 'sm' : 'md'}`}
+                className="!text-assistive-strong">
+                매물 등록하기
+                <CaretRight size={isMobile ? 16 : 24} className="ml-[10px]" />
+              </Button>
+            </div>
+          </div>
         </div>
-        <div>
-          {/* 테블릿 버전 list 크기 이상 문제 해결하기 */}
-          {data?.content?.properties.length > 0 && (
-            <div className="flex flex-col gap-6 mb-12 tablet:mb-11 mobile:mb-9">
-              <div>
+        {data?.content?.properties.length > 0 && (
+          <div className="">
+            <div className="mb-12 tablet:mb-11 mobile:mb-9">
+              <div className="mb-6 mobile:mb-9">
                 <h1 className="text-title-2xl mobile:text-title-lg-m font-bold">전국 미분양 매물 TOP 20</h1>
                 <p className="text-detail-lg mobile:text-detail-base-m">
                   표시되는 가격은 최소 평형 최저가입니다
                 </p>
               </div>
-              {data?.content?.properties.map((property: any, index: any) => {
-                const commonProps = {
-                  imageUrl: property.image_url,
-                  title: property.property_name,
-                  address: property.area_addr,
-                  propertyType: property.property_type,
-                  salesType: property.sales_type,
-                  totalNumber: property.total_number,
-                  keywords: property.keywords,
-                  price: property.price,
-                  discountPrice: property.discount_price,
-                  like: property.like,
-                  rank: index + 1 + (currentPage - 1) * 5,
-                };
+              <div className="flex flex-col gap-6 items-center">
+                {data?.content?.properties.map((property: any, index: any) => {
+                  const commonProps = {
+                    imageUrl: property.image_url,
+                    title: property.property_name,
+                    address: property.area_addr,
+                    propertyType: property.property_type,
+                    salesType: property.sales_type,
+                    totalNumber: property.total_number,
+                    keywords: property.keywords,
+                    price: property.price,
+                    discountPrice: property.discount_price,
+                    like: property.like,
+                    rank: index + 1 + (currentPage - 1) * 5,
+                  };
 
-                return isMobile ? (
-                  <ItemCard key={property.id} size="s" {...commonProps} />
-                ) : (
-                  <ItemList key={property.id} size={isTablet ? 'm' : 'l'} {...commonProps} />
-                );
-              })}
+                  return isMobile ? (
+                    <ItemCard key={property.id} size="s" {...commonProps} />
+                  ) : (
+                    <ItemList key={property.id} size={isTablet ? 'm' : 'l'} {...commonProps} />
+                  );
+                })}
+              </div>
             </div>
-          )}
-          <DefaultPagination totalPages={totalPages} onPageChange={setCurrentPage} />
-        </div>
+            <DefaultPagination
+              totalPages={totalPages}
+              currentPage={currentPage}
+              onPageChange={setCurrentPage}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
