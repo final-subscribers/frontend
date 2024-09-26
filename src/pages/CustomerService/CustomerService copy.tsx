df451fddaa78dfe79d32c245a3ff88141956290e
scripto1 <heeyongi@gmail.com>
2024-09-26 18:37:36 +0900

    refactor: 상담사api수정

--- src/pages/CustomerService/CustomerService copy.tsx
+++ src/pages/CustomerService/CustomerService copy.tsx
@@ -1,7 +1,7 @@
-import React, { ComponentType } from 'react';
-import { ConsultingPending } from '@/components/CustomerService/ConsultingPending';
+import { ComponentType, useEffect } from 'react';
+import { ConsultingPending } from '@/components/Table/ConsultingPending';
 import { CustomerData } from '@/types/types';
-import { ConsultingCompleted } from '@/components/CustomerService/ConsultingCompleted';
+import { ConsultingCompleted } from '@/components/Table/ConsultingCompleted';
 import { columnsPending } from '@/components/ui/columnsPending';
 import { columnsCompleted } from '@/components/ui/columnsCompleted';
 import CustomerInquiry from '@/components/CustomerService/CustomerInquiry';
@@ -16,48 +16,101 @@ import AccordionMenu from '@/components/CustomerService/AccordionMenu';
 import { propertyTypeMapping } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import NewCustomer from '@/components/CustomerService/NewCustomer';
-import { keepPreviousData, QueryClientProvider, useQuery } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { useState, useRef } from 'react';
 import { Dialog, DialogContent, DialogTrigger, DialogDescription } from '@/components/ui/dialogNewCustomer';
 import NoProperty from '../../components/CustomerService/NoProperty';
-import { useQueryClient } from '@tanstack/react-query';
-import { fetchSidebarData, fetchSidebarDetailData } from '@/api/consulting';
+import { useQueries, useQueryClient } from '@tanstack/react-query';
+import { fetchSidebarData, fetchPendingConsultations, fetchCompletedConsultations } from '@/api/consulting';
 
-function CustomerService() {
+export default function CustomerService() {
   const [isOpen, setIsOpen] = useState(false);
   const [customers, setCustomers] = useState<CustomerData[]>([]);
-  const [selectedProperty, setSelectedProperty] = useState<number>();
+  const [selectedProperty, setSelectedProperty] = useState({
+    id: 49,
+    image: 'https://www.sjsori.com/news/photo/202011/46528_56925_4153.jpg',
+    propertyName: '한림풀에버',
+    companyName: '(주)건설개발',
+    constructor: '쌍용건설',
+    totalNumber: 100,
+    startDate: '2024-06-22',
+    endDate: '2024-10-30',
+    propertyType: 'APARTMENT',
+  });
   const popupWindowRef = useRef<Window | null>(null);
+
+  const [selectedConsultant, setSelectedConsultant] = useState<string>('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [date, setDate] = useState<Date | undefined>();
+  // @ts-ignore: Unused variable
+  const [pageSize, setPageSize] = useState(10);
 
   const queryClient = useQueryClient();
 
-  // TODO:customers 사용안해서 임시로 넣어둔 것.
-  console.log(customers);
-
-  // 사이브바 fetch
-  const { data: sidebarData } = useQuery({
-    queryKey: ['sidebar'],
-    queryFn: fetchSidebarData,
+  const results = useQueries({
+    queries: [
+      {
+        queryKey: [
+          'pendingConsultations',
+          {
+            propertyId: selectedProperty.id || 49,
+            search: '',
+            consultant: selectedConsultant,
+            preferredAt: date,
+            page: 0,
+          },
+        ],
+        queryFn: fetchPendingConsultations,
+      },
+      {
+        queryKey: [
+          'completedConsultations',
+          {
+            propertyId: selectedProperty.id || 49,
+            search: '',
+            tier: '',
+            consultant: '',
+            preferredAt: undefined,
+            page: 0,
+          },
+        ],
+        queryFn: fetchCompletedConsultations,
+      },
+      {
+        queryKey: ['sidebarData', { propertyId: selectedProperty.id || 49 }],
+        queryFn: fetchSidebarData,
+      },
+    ],
   });
 
-  // 매물 상세(상단) fetch
-  const { data: sidebarDetailData } = useQuery({
-    queryKey: ['sidebarDetail', selectedProperty!],
-    queryFn: () => fetchSidebarDetailData(selectedProperty!),
-    placeholderData: keepPreviousData,
-  });
+  const [pendingConsultationsResult, completedConsultationsResult, sidebarDataResult] = results;
 
-  // selectedProperty 초기화
-  React.useEffect(() => {
-    if (!selectedProperty && sidebarData?.sideBarPendingResponseList?.length > 0) {
-      setSelectedProperty(sidebarData.sideBarPendingResponseList[0].id);
-    }
-  }, [sidebarData, selectedProperty]);
+  const isLoading =
+    pendingConsultationsResult.isLoading ||
+    completedConsultationsResult.isLoading ||
+    sidebarDataResult.isLoading;
+  const error =
+    pendingConsultationsResult.error || completedConsultationsResult.error || sidebarDataResult.error;
+
+  const pendingConsultationsData = pendingConsultationsResult.data?.consultPendingSummaries || [];
+  const completedConsultationsData = completedConsultationsResult.data?.consultCompletedSummaries || [];
+  const sidebarData = sidebarDataResult.data;
+
+  const pendingCustomersData = pendingConsultationsData;
+  const completedCustomersData = completedConsultationsData;
+
+  // console.log(pendingCustomersData); // Debug log
 
   const handleAddNewCustomer = (newCustomer: CustomerData) => {
     setCustomers((prevCustomers) => [...prevCustomers, newCustomer]);
     setIsOpen(false);
+    console.log(customers);
+    console.log(pendingCustomersData);
   };
+
+  // const pendingCustomers = customers.filter((customer) => customer.status === 'pending');
+  // const completedCustomers = customers.filter((customer) => customer.status === 'complete');
+
   const accordionSections = [
     {
       title: '모집중',
@@ -78,7 +131,7 @@ function CustomerService() {
   ];
 
   const handlePropertySelect = (id: number) => {
-    if (selectedProperty !== id) {
+    if (selectedProperty.id !== id) {
       const selectedFromPending = sidebarData?.sideBarPendingResponseList.find(
         (property: { id: number }) => property.id === id,
       );
@@ -88,10 +141,43 @@ function CustomerService() {
       const selected = selectedFromPending || selectedFromCompleted;
 
       if (selected) {
-        setSelectedProperty(selected.id);
+        setSelectedProperty((prevState) => ({
+          ...prevState,
+          id: selected.id,
+        }));
       }
     }
   };
+
+  useEffect(() => {
+    if (selectedProperty.id) {
+      queryClient.invalidateQueries({ queryKey: ['sidebarData'] });
+      queryClient.invalidateQueries({ queryKey: ['pendingConsultations'] });
+      queryClient.invalidateQueries({ queryKey: ['completedConsultations'] });
+    }
+  }, [selectedProperty.id, queryClient]);
+
+  useEffect(() => {
+    if (sidebarData?.sideBarSelectedPropertyResponse) {
+      const selectedPropertyDetails = sidebarData.sideBarSelectedPropertyResponse;
+      setSelectedProperty((prevState) => ({
+        ...prevState,
+        image: selectedPropertyDetails.image,
+        propertyName: selectedPropertyDetails.name,
+        companyName: selectedPropertyDetails.companyName,
+        constructor: selectedPropertyDetails.constructor,
+        totalNumber: selectedPropertyDetails.totalNumber,
+        startDate: selectedPropertyDetails.startDate,
+        endDate: selectedPropertyDetails.endDate,
+        propertyType: selectedPropertyDetails.propertyType,
+      }));
+    }
+  }, [sidebarData]);
+
+  // const filteredPendingCustomers = pendingCustomers.filter((customer) => customer.id === selectedProperty.id);
+  // const filteredCompletedCustomers = completedCustomers.filter(
+  //   (customer) => customer.id === selectedProperty.id,
+  // );
 
   const isSidebarDataEmpty =
     !sidebarData ||
@@ -199,59 +285,47 @@ function CustomerService() {
   };
   const handleCompletedClick = (props: CustomerCompletedProps) => {
     openPopupWindow(CustomerCompleted, props, 'Completed');
+    console.log(props);
   };
 
   return (
-    <main className="flex gap-10">
-      <aside className="flex-none flex flex-col w-[280px] mt-7 ml-7 ">
+    <main className="flex">
+      <aside className="flex flex-col w-[280px] mt-7 ml-7">
         <div className="flex py-6 px-6 gap-6 border-b-[1px] border-assistive-alternative">
           <ListDashes size={24} weight="light" />
           <h1 className="text-label-lg font-bold text-static-default ">매물 전체보기</h1>
         </div>
-        <AccordionMenu
-          sections={accordionSections}
-          onItemSelect={handlePropertySelect}
-          selectedProperty={selectedProperty}
-        />
+        <AccordionMenu sections={accordionSections} onItemSelect={handlePropertySelect} />
       </aside>
 
       {isSidebarDataEmpty ? (
         <NoProperty />
       ) : (
         <section className="container mx-auto py-10 ">
-          <article className="flex flex-col mb-10 gap-7">
-            <div>
-              <p className="ml-3 text-body-lg font-normal text-assistive-strong">
-                {propertyTypeMapping[sidebarDetailData?.propertyType]}
-              </p>
-              <h1 className="ml-3 text-title-2xl font-bold">{sidebarDetailData?.propertyName}</h1>
-            </div>
-            <div className="flex justify-between">
-              <div className="flex gap-10">
-                <img
-                  src={sidebarDetailData?.image}
-                  className="flex-none object-cover w-[320px] h-[180px] rounded-5"
-                />
-                <div className="flex gap-4">
-                  <div className="flex flex-col self-center w-[64px] gap-3 text-detail-base text-assistive-strong">
-                    <p>시행사</p>
-                    <p>시공사</p>
-                    <p>세대수</p>
-                    <p>모집기간</p>
-                  </div>
-                  <div className="flex flex-col self-center w-[542px] gap-3 text-detail-base text-static-default">
-                    <p>{sidebarDetailData?.companyName}</p>
-                    <p>{sidebarDetailData?.constructor}</p>
-                    <p>{sidebarDetailData?.totalNumber}</p>
-                    <p>
-                      {sidebarDetailData?.startDate} - {sidebarDetailData?.endDate}
-                    </p>
-                  </div>
-                </div>
+          <article className="flex flex-col mb-10">
+            <p className="ml-3 text-body-lg font-normal text-assistive-strong">
+              {propertyTypeMapping[selectedProperty.propertyType]}
+            </p>
+            <h1 className="ml-3 text-title-2xl font-bold">{selectedProperty.propertyName}</h1>
+            <div className="flex">
+              <img src={selectedProperty.image} className="object-cover w-[320px] h-[180px]" />
+              <div className="flex flex-col self-center ml-10 w-[64px] gap-3 text-detail-base text-assistive-strong">
+                <p>시행사</p>
+                <p>시공사</p>
+                <p>세대수</p>
+                <p>모집기간</p>
+              </div>
+              <div className="flex flex-col self-center ml-4 w-[542px] gap-3 text-detail-base text-static-default">
+                <p>{selectedProperty.companyName}</p>
+                <p>{selectedProperty.constructor}</p>
+                <p>{selectedProperty.totalNumber}</p>
+                <p>
+                  {selectedProperty.startDate}-{selectedProperty.endDate}
+                </p>
               </div>
               <Dialog open={isOpen} onOpenChange={setIsOpen}>
-                <div className="self-end mr-11">
-                  <DialogTrigger asChild>
+                <div className="self-end">
+                  <DialogTrigger>
                     <Button variant="primary" size="lg" className="gap-4">
                       고객추가
                       <Plus size={24} weight="bold" />
@@ -259,7 +333,10 @@ function CustomerService() {
                   </DialogTrigger>
                 </div>
                 <DialogContent>
-                  <NewCustomer onAddCustomer={handleAddNewCustomer} propertyId={sidebarDetailData?.id} />
+                  <NewCustomer
+                    onAddCustomer={handleAddNewCustomer}
+                    propertyId={selectedProperty.id.toString()}
+                  />
                   <DialogDescription />
                 </DialogContent>
               </Dialog>
@@ -273,14 +350,30 @@ function CustomerService() {
             </TabsList>
             <TabsContent value="pending">
               <ConsultingPending
-                columns={columnsPending(handleInquiryClick, selectedProperty)}
-                propertyId={selectedProperty}
+                columns={columnsPending(handleInquiryClick, selectedProperty.id)}
+                data={pendingCustomersData}
+                isLoading={isLoading}
+                error={error}
+                selectedConsultant={selectedConsultant}
+                setSelectedConsultant={setSelectedConsultant}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                date={date}
+                setDate={setDate}
               />
             </TabsContent>
             <TabsContent value="completed">
               <ConsultingCompleted
                 columns={columnsCompleted(handleCompletedClick)}
-                propertyId={selectedProperty}
+                data={completedCustomersData}
+                selectedConsultant={selectedConsultant}
+                setSelectedConsultant={setSelectedConsultant}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                date={date}
+                setDate={setDate}
+                totalPages={completedConsultationsResult.data?.totalPages || 1}
+                pageSize={pageSize}
               />
             </TabsContent>
           </Tabs>
@@ -289,5 +382,3 @@ function CustomerService() {
     </main>
   );
 }
-
-export default CustomerService;
